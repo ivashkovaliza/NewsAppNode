2214b2698521d26fb8206cd77ecc796ac92ba6e8
ivashkovaliza <31897547+ivashkovaliza@users.noreply.github.com>
2019-11-28 22:42:00 +0300

    Add node express hw-1 (#1)
    * Add node express hw-1
    
    * Edit node express hw-1

---
+++ src/newsController.js
@@ -0,0 +1,52 @@
+const news = require('./news');
+
+function getAllNews(req, res, next) {
+  res.send(news);
+}
+
+function getNewsItemById(req, res, next) {
+  const id = req.params.id;
+
+  res.send(news.articles[id]);
+}
+
+function deleteNewsItemById(req, res, next) {
+  const id = req.params.id;
+
+  news.articles.splice(id, 1);
+  res.json({ success: 'ok'});
+}
+
+function changeNewsTitle(req, res, next) {
+  const id = req.params.id;
+
+  news.articles[id].title = req.body.title;
+  res.json({ success: 'ok'});
+}
+
+function hasTitle(req, res, next) {
+  const newTitle = req.body.title;
+
+  if(newTitle) {
+    next()
+  } else {
+    res.status(422).send({ error: `There is no title value in request body!` });
+  }
+}
+
+function checkId(req, res, next) {
+  const id = req.params.id;
+
+  if(news.articles[id]) {
+    next();
+  } else {
+    res.status(404).send({ error: `Element with ${id} id couldn't be found!` });
+  }
+}
+
+function postNewNewsItem(req, res, next) {
+    news.articles.push(req.body);
+    res.json({ success: 'ok'});
+}
+
+module.exports = { getNewsItemById,  deleteNewsItemById, changeNewsTitle, getAllNews, checkId, hasTitle, postNewNewsItem };
