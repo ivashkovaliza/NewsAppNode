b24686d1df4ca668fe54381f9a6b64e1ccb2d22f
Elizaveta Ivashkova <Elizaveta_Ivashkova@epam.com>
2019-12-04 21:03:28 +0300

    Add node express mongoose hw-2

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # news-app-node
 -----------------------------------
 
+### Install MongoDB
+
+https://docs.mongodb.com/manual/administration/install-community/
+
+1. Create "fronctcamp" database
+2. Add "articles" and "users" collections
+3. Import data from users.json and news.json  
+
 ### Install:
 
 $ npm install
@@ -8,3 +16,6 @@ $ npm install
 ### Launch node.js server:
 
 $npm start
+
+
+
--- src/newsRoutes.js
+++ src/newsRoutes.js
@@ -1,14 +1,44 @@
 const express = require('express');
-const { getNewsItemById,  deleteNewsItemById, changeNewsTitle, getAllNews, checkId, hasTitle, postNewNewsItem} = require('./newsController');
+const { getNewsItemById,  deleteNewsItemById, editNewsTitleById, getAllNews, postNewNewsItem } = require('./controllers/newsController');
+const registerUser = require('./controllers/usersController');
 const router = express.Router();
+const passport = require('passport');
+
+require('./config/basic');
+require('./config/facebook');
+
+router.route('/auth/facebook')
+  .get(passport.authenticate('facebook'));
+
+router.route('/auth/facebook/callback')
+  .get(passport.authenticate("facebook", {
+    successRedirect: "/",
+    failureRedirect: "/fail"
+  }));
+
+router.route('/fail')
+  .get((req, res) => {
+    res.send("Failed attempt");
+  });
+
+router.route('/')
+  .get((req, res) => {
+    res.send("Success");
+  });
 
 router.route('/news')
-  .get(getAllNews)
-  .post(hasTitle, postNewNewsItem);
+  .get( getAllNews)
+  .post(postNewNewsItem);
 
 router.route('/news/:id')
-  .get(checkId, getNewsItemById)
-  .put(checkId, hasTitle, changeNewsTitle)
-  .delete(checkId, deleteNewsItemById);
+  .get(getNewsItemById)
+  .put(passport.authenticate('basic', {session: false}), editNewsTitleById)
+  .delete(deleteNewsItemById); //passport.authenticate('facebook')
+
+router.route('/register')
+  .post(registerUser);
+
+router.route('/auth/facebook/callback')
+  .get((req, res) => {res.send('this check status of the request')});
 
 module.exports = router;
